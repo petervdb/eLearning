793afa4156d7d8b35fe9f9d4b009b7459a51bcf8
Peter Vandenberghe <petervanden@gmail.com>
2014-10-21 09:23:43 +0200

    Completed ex20 Python hardway course

---
+++ python/hardway/ex20.py
@@ -0,0 +1,41 @@
+from sys import argv
+
+# variable script is the name of the script
+
+script, input_file = argv
+
+def print_all(f):
+	print f.read()
+
+def rewind(f):
+	f.seek(0)
+
+def print_a_line(line_count, f):
+	print line_count, f.readline()
+
+# open the file
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+print "Let's print three lines:"
+
+# print the first line
+current_line = 1
+print_a_line(current_line, current_file)
+
+# print the second line
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+# print the third line
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+print"Then name of this script is %s" % script
