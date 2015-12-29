37a2d590559688d321c90fd46eb3d2baccd34c6f
Peter Vandenberghe <petervanden@gmail.com>
2015-12-29 10:46:11 +0100

    Started with chapter 37 Learn Python the hard way

---
+++ python/hardway/ex37_1.py
@@ -0,0 +1,9 @@
+# Exercise 37
+# Some code to review Keywords, Data Types, String Escape Sequences, String formats, Operators
+
+def start():
+	print "This script contains code on how to use Keywords, Data Types, String Escape Sequences, String formats and Operators"
+
+# main part
+
+start()
