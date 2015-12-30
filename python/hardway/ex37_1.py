57f7e3c89f6b368eed56b314cf76025fdd1fd2f1
Peter Vandenberghe <petervanden@gmail.com>
2015-12-30 08:14:48 +0100

    ex37

--- python/hardway/ex37_1.py
+++ python/hardway/ex37_1.py
@@ -1,6 +1,9 @@
 # Exercise 37
 # Some code to review Keywords, Data Types, String Escape Sequences, String formats, Operators
 
+def check_user():
+	print "We need to verify who you are before you can use this program."
+
 def start():
 	print "This script contains code on how to use Keywords, Data Types, String Escape Sequences, String formats and Operators"
 
