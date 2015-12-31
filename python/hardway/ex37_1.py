aa526e5ca0038f031882f359d74428a2a2904606
Peter Vandenberghe <petervanden@gmail.com>
2015-12-31 18:06:29 +0100

    ex37 Python course

--- python/hardway/ex37_1.py
+++ python/hardway/ex37_1.py
@@ -1,5 +1,24 @@
 # Exercise 37
 # Some code to review Keywords, Data Types, String Escape Sequences, String formats, Operators
+# ToDo
+# Keywords:
+# and, del, from, not, while, as, elif, global, or, with, assert, else, if
+# pass, yield, break,except, import, print, class, exec, in, raise, continue
+# finally, is, return, def, for, lambda, try
+#
+# DataTypes:
+# True, False, None, strings, numbers, floats, lists
+#
+# String Escape Seequences
+# \\, \', \", \a, \b, \f, \n, \r \t, \v
+#
+# String Formats
+# %d, %i, %o, %u, %x, %e, %E, %f, %F, %g, %G, %c, %r, %s, %%
+#
+# Operator
+# +, -, *, **, /, //, %, <, >, <=, >=, ==, !=, <>, ( ), [ ], { }, @, , :, . , =, ;, +=, -=, *=, /=, //=, %=, **=
+
+
 from time import sleep
 
 def check_user():
