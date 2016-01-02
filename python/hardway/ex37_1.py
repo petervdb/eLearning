9c9c12ab83838c5c207af7b2d674ab1bcc7bac13
Peter Vandenberghe <petervanden@gmail.com>
2016-01-02 08:59:47 +0100

    ex37 Python course

--- python/hardway/ex37_1.py
+++ python/hardway/ex37_1.py
@@ -1,42 +1,57 @@
 # Exercise 37
 # Some code to review Keywords, Data Types, String Escape Sequences, String formats, Operators
-# ToDo
-# Keywords:
+
+# ToDo Keywords :
 # and, del, from, not, while, as, elif, global, or, with, assert, else, if
-# pass, yield, break,except, import, print, class, exec, in, raise, continue
-# finally, is, return, def, for, lambda, try
+# pass, yield, break,except, import, print, class, exec, raise, continue
+# finally, is, return, def, lambda, try
 #
-# DataTypes:
+# DONE Keywords :
+# for, in,
+#
+# ToDo DataTypes:
 # True, False, None, strings, numbers, floats, lists
 #
-# String Escape Seequences
+# Done DataTypes:
+#
+#
+# ToDo String Escape Seequences
 # \\, \', \", \a, \b, \f, \n, \r \t, \v
 #
-# String Formats
-# %d, %i, %o, %u, %x, %e, %E, %f, %F, %g, %G, %c, %r, %s, %%
+# Done String Escape Seequences
 #
-# Operator
+#
+# ToDo String Formats
+# %d, %i, %o, %u, %x, %e, %E, %f, %F, %g, %G, %c, %r, %%
+#
+# Done String Formats
+# %s,
+#
+# ToDo Operator
 # +, -, *, **, /, //, %, <, >, <=, >=, ==, !=, <>, ( ), [ ], { }, @, , :, . , =, ;, +=, -=, *=, /=, //=, %=, **=
+#
+# Done Operator
+#
 
 
 from time import sleep
 
 def check_user():
-        print "We need to verify who you are before you can use this program."
-        you = raw_input("So, tell me, who are you? ")
-        youlow = you.lower()
-        # print youlow
-        print ("Let's check the database if user %s exists ..." % you)
-        # Count to 10 simulating database search
-        for i in range(0, 10):
-                sleep(0.1)
-                print("."),
-        print " "
-        print ("Haha, I found you. OK %s, we can continue with this program" % you)
+    print "We need to verify who you are before you can use this program."
+    you = raw_input("So, tell me, who are you? ")
+    youlow = you.lower()
+    # print youlow
+    print ("Let's check the database if user %s exists ..." % you)
+    # Count to 10 simulating database search
+    for i in range(0, 10):
+            sleep(0.1)
+            print("."),
+    print " "
+    print ("Haha, I found you. OK %s, we can continue with this program" % you)
         
 def start():
-        print "This script contains code on how to use Keywords, Data Types, String Escape Sequences, String formats and Operators"
-        check_user()
+    print "This script contains code on how to use Keywords, Data Types, String Escape Sequences, String formats and Operators"
+    check_user()
 
 # main part
 
