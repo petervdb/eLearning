d494747c8087665b9a64d198073cd9286e69b17f
Peter Vandenberghe <petervanden@gmail.com>
2016-01-04 09:59:45 +0100

    ex37 Python course

--- python/hardway/ex37_1.py
+++ python/hardway/ex37_1.py
@@ -2,16 +2,16 @@
 # Some code to review Keywords, Data Types, String Escape Sequences, String formats, Operators
 
 # ToDo Keywords:
-# and, del, not, while, as, elif, or, with, assert, pass, yield, break,except, class, exec, raise, continue, finally, is, return, lambda, try
+# and, del, not, as, elif, or, with, assert, pass, yield, break,except, class, exec, raise, continue, finally, is, lambda, try
 #
 # DONE Keywords:
-# for, in, from, if, else, import, print, def, global
+# for, in, from, if, else, import, print, def, global, while
 #
 # ToDo DataTypes:
-# None, strings, numbers, floats, lists
+# None, lists
 #
 # Done DataTypes:
-# True, False
+# True, False, strings, numbers, floats
 #
 # ToDo String Escape Seequences
 # \\, \', \", \a, \b, \f, \n, \r, \t, \v
@@ -23,7 +23,7 @@
 # %d, %i, %o, %u, %x, %e, %E, %f, %F, %g, %G, %c, %r, %%
 #
 # Done String Formats
-# %s,
+# %s, %d
 #
 # ToDo Operator
 # +, -, *, **, /, //, %, <, >, <=, >=, ==, !=, <>, ( ), [ ], { }, @, , :, . , =, ;, +=, -=, *=, /=, //=, %=, **=
@@ -34,8 +34,15 @@
 from time import sleep
 from sys import exit
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 def check_user():
-    global user_is_known
+    global user_is_known, you
     if user_is_known == False:
         print "We need to verify who you are before you can use this program."
         you = raw_input("So, tell me, who are you? ")
@@ -51,12 +58,29 @@ def check_user():
         user_is_known = True
     else:
         print "Yes, you are already known by the system"
-        
+
+def check_age(age_is_known):
+    if age_is_known == False:
+        print "It look like you're age is unknown by the system."
+        your_age = raw_input("So, tell me, how old are you? ")
+        while is_number(your_age) == False:
+            print "Don't you know the difference between a string and a number?"
+            your_age = raw_input("Let's try again, how old are you? ")
+
+    else:
+        print("Your age is known in the database. Great!")
+    return(int(your_age))
+
 def start():
+    global user_is_known, age_is_known, you
     print "This script contains code on how to use Keywords, Data Types, String Escape Sequences, String formats and Operators"
     check_user()
     check_user() # Just check again
+    age_user = check_age(age_is_known)
+    print"So, %s you are %d years old." % (you, age_user)
 
 # main part
 user_is_known = False
+age_is_known = False
+you = "unknown"
 start()
